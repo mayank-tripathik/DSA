345d9113fee0ee3566cdfaba5a925cfa07d4fcc5
mayank-tripathik <mayanktripathik@gmail.com>
2016-09-05 15:59:22 +0530

    Update count_inversions.cpp

--- Array-Problems/count_inversions.cpp
+++ Array-Problems/count_inversions.cpp
@@ -64,3 +64,48 @@ int main(){
 			cout<<arr[j]<<" ";
 	}
 }
+
+
+
+
+
+/*------------------------Time Complexity : O(nlogn) -------------------------*/
+/*------------------Here STL map is used to keep track of both array elements and their indices------------------------------*/
+
+
+
+#include<iostream>
+#include<map>
+#include<algorithm>
+#include<utility>
+using namespace std;
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+		int n,i,x;
+		cin>>n;
+		map<int,int> arr;
+		for(i=0;i<n;i++){
+			cin>>x;
+			arr.insert(make_pair(x,i+1));
+		}
+		map<int,int> :: iterator itr,temp,it;
+		for(it=arr.begin();it!=arr.end();it++){
+			cout<<(*it).first<<":"<<(*it).second<<endl;
+		}
+		temp=arr.begin();
+		temp++;
+		cout<<"Inversion Pairs:\n";
+		for(itr=arr.begin();temp!=arr.end();itr++)
+		{ 
+			if((*itr).second>(*temp).second)
+				cout<<(*temp).first<<" and "<<(*itr).first<<endl;
+			temp++;
+		}
+	}
+		
+}
+

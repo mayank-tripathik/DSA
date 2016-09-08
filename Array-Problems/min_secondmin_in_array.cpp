cd4e77b10bfd29213045fc5a46a7aff42ea35199
mayank-tripathik <mayanktripathik@gmail.com>
2016-09-08 23:11:02 +0530

    Update min_secondmin_in_array.cpp

--- Array-Problems/min_secondmin_in_array.cpp
+++ Array-Problems/min_secondmin_in_array.cpp
@@ -2,22 +2,22 @@
 #include<cstdlib>
 using namespace std;
 
-void find_min(int *arr, int n){
-	if(n==1){
-		cout<<"Min:"<<arr[0]<<endl;
+void find_min(int *arr, int low, int high){
+	if(high-low+1==1){
+		cout<<"Min:"<<arr[low]<<endl;
 		cout<<"No second min exist"<<endl;
 	}
 	else{
 		int min,second_min;
-		if(arr[0]<arr[1]){
-			min=0;
-			second_min=1;
+		if(arr[low]<arr[low+1]){
+			min=low;
+			second_min=low+1;
 		}
 		else{
-			min=1;
-			second_min=0;
+			min=low+1;
+			second_min=low;
 		}
-		for(int i=2;i<n;i++){
+		for(int i=low+2;i<=high;i++){
 			if(arr[i]<arr[min]){
 				second_min=min;
 				min=i;
@@ -39,5 +39,5 @@ int main(){
 		arr[i]=x;
 	}
 	cout<<endl;
-	find_min(arr, n);
+	find_min(arr, 0, n-1);
 }

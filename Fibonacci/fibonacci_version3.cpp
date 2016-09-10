7f4e7b0e95ae731dea1544b78fbecb8d69fdf07a
mayank-tripathik <mayanktripathik@gmail.com>
2016-09-10 19:20:48 +0530

    Update fibonacci_version3.cpp

--- Fibonacci/fibonacci_version3.cpp
+++ Fibonacci/fibonacci_version3.cpp
@@ -7,10 +7,16 @@
  */
  
  
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-#include<cstdlib>
+#include <algorithm>
 using namespace std;
-int dlsb,dmsb,blsb,bmsb;
+
+
+int dmsb,dlsb,bmsb,blsb;
+
 int A[2][2]={{1,1},{1,0}};
 int result[2][2]={{1,0},{0,1}};
 
@@ -26,89 +32,92 @@ void matrix_multiply(int M1[][2], int M2[][2], long k)
             M1[i][j] = temp[i][j] % k;
 }
 
-void div_by_two(){
-	blsb--;
-}
-
-bool num_greater_zero(char call){
-	if(call=='d'){
-		if (dmsb> dlsb|| num[dmsb] == 0)
-			return 0;
-		else
-			return 1;
-	}
-	else{
-		if (bmsb> blsb|| num[bmsb] == 0)
-			return 0;
-		else
-			return 1;
-		
-	}
-	
-}
-
-void matrix_power(long *num,long n,long k){
-	while(num_greater_zero('b')){
-		if(num[lsb]==1)
-			matrix_multiply(result,A,k);
-		matrix_multiply(A,A,k);
-		div_by_two();
-	}
-}
-
-
-int fib(long *bnum, long n,long k){
-	matrix_power(bnum,n,k);
-	return result[1][0];
-}
-
-void div_by_two_decimal{
-	long carry=0;
-	for(int i=0;i<n;i++){
-		int next_digit=arr[i]+carry;
-		arr[i]=next_digit/2;
-		if(next_digit&1)
-			carry=10;
-		else
-			carry=0;
-	}
-    while(arr[i]==0 && dmsb<=dlsb)
+void initialize_index(int n, int *dnum){
+    dmsb=0;
+    while(dnum[dmsb]==0)
         dmsb++;
-    if(carry==10)
-        return 1;
-    else
-        return 0;
+    dlsb=n-1;
+    bmsb=4*n-1;
+    blsb=4*n-1;
 }
 
-void decimal_to_binary(int *dnum, int *bnum, int n)
-{
-    while(greater_than_zero('d')){
-        if(divide_by_two_decimal(dnum,n))
+bool divide_by_two(int *dnum){
+    int carry=0;
+    for(int i=dmsb;i<=dlsb;i++){
+        int next_digit=dnum[i]+carry;
+        dnum[i]=next_digit/2;
+        if(next_digit&1)
+            carry=10;
+        else
+            carry=0;
+    }
+    while(dnum[dmsb]==0)
+        dmsb++;
+    if(carry==0)
+        return false;
+    else
+        return true;
+}
+
+void decimal_to_binary(int *dnum, int *bnum){
+    while(dmsb<=dlsb){
+        bool carry_generated=divide_by_two(dnum);
+        if(carry_generated)
             bnum[bmsb]=1;
         else
             bnum[bmsb]=0;
         bmsb--;
-        
     }
+    bmsb++;
+}
+
+bool binary_num_greater_than_zero(){
+    if(bmsb<=blsb)
+        return true;
+    else
+        return false;
+}
+
+bool number_is_odd(int *bnum){
+    if(bnum[blsb]==1)
+        return true;
+    else
+        return false;
+}
+
+void matrix_power(int *bnum,long m){
+    while(binary_num_greater_than_zero()){
+        if(number_is_odd(bnum))
+            matrix_multiply(result,A,m);
+        matrix_multiply(A,A,m);
+        blsb--;
+        }
+}
+
+
+int fib(int *bnum, long m){
+    matrix_power(bnum,m);
+    return result[1][0];
 }
 
 int main() {
-	long n,k;
-	cin>>n;
-	cin>>k;
-	long dnum[n];
-    long bnum[n*4];
-    dlsb=n-1;
-    dmsb=0;
-    blsb=n*4-1;
-    bmsb=n*4-1;
-	for(int i=0;i<n;i++){
-		int x=rand()%n;
-		dnum[i]=x;
-	}
-	while(dnum[dmsb]==0)
-		dmsb++;
-    decimal_to_binary(dnum,bnum,n);
-	cout<<fib(bnum,n,k);
-	return 0;
+    long n;  //length of the number of whose fibonacci is to be calculated
+    long m;
+    cin>>n;
+    int dnum[n];
+    int bnum[4*n];
+    cin>>m;
+    srand(time(NULL));
+    for(int i=0;i<n;i++){
+        int x=rand()%10;
+        dnum[i]=x;
+        //cout<<dnum[i]<<" ";
+    }
+    initialize_index(n,dnum);
+    decimal_to_binary(dnum,bnum);
+    /*for(int i=bmsb;i<=blsb;i++)
+        cout<<bnum[i]<<" ";*/
+    //cout<<endl;
+    cout<<fib(bnum,m);
+    return 0;
 }

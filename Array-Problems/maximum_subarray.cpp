9298146b84fe708596483c947f4cf88b6aa2ed9c
mayank-tripathik <mayanktripathik@gmail.com>
2016-09-13 21:09:22 +0530

    Update maximum_subarray.cpp

--- Array-Problems/maximum_subarray.cpp
+++ Array-Problems/maximum_subarray.cpp
@@ -1,31 +1,32 @@
-#include<iostream>
+/*
+ * Problem : To find the sum of contiguous subarray within a one-dimensional array of numbers which has the largest sum 
+ * Kadane's Algorithm
+ * Time Complexity: O(n)
+ * Following Implimentation works for all negative numbers too
+ */
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+
+int max_sum_contiguous(int *arr, int n){
+    int max_sum=arr[0],current_sum=arr[0];
+    for(int i=1;i<n;i++){
+        current_sum=max(arr[i],current_sum+arr[i]);
+        max_sum=max(current_sum,max_sum);
+    }
+    return max_sum;
+}
 int main(){
-	int test;
-	cin>>test;
-	while(test--)
-	{
-		int n,i,j,k,current_sum,max_sum;
-		cin>>n;
-		int arr[n];
-		for(i=0;i<n;i++)
-			cin>>arr[i];
-		i=0;
-		max_sum=current_sum=0;
-		for(k=0;k<n;k++)
-		{
-			current_sum+=arr[k];
-			if(current_sum>max_sum){
-				max_sum=current_sum;
-				j=k;
-				}
-			if(current_sum<0)
-			{
-				current_sum=0;
-				i=k+1;
-			}
-		}
-		cout<<"Maximum:"<<max_sum<<endl;
-		cout<<"i:"<<i<<" and j:"<<j<<endl;  //i and j store index between which maximum sum is found.
-	}
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<max_sum_contiguous(arr,n)<<endl;
 }

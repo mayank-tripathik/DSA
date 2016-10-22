df3a7acffb60dddd019137be82664c84b1990d4c
mayank-tripathik <mayanktripathik@gmail.com>
2016-10-23 00:20:37 +0530

    Update dfs_recursive.cpp

--- Graph/dfs_recursive.cpp
+++ Graph/dfs_recursive.cpp
@@ -18,14 +18,14 @@ int main()
 {
 	int u,v,start,nodes,edges;
 	cin>>nodes>>edges;
-    vector<list<int> >graph(nodes+1);
-    vector<bool> visited(nodes+1,false);
-    for(int i=1;i<=edges;i++){
-    	cin>>u>>v;
-    	graph[u].push_back(v);
-    	graph[v].push_back(u);
-    }
-    cin>>start;
-    dfs(graph,start,visited);
-    return 0;
+	vector<list<int> >graph(nodes+1);
+        vector<bool> visited(nodes+1,false);
+        for(int i=1;i<=edges;i++){
+    	    cin>>u>>v;
+    	    graph[u].push_back(v);
+    	    graph[v].push_back(u);
+        }
+        cin>>start;
+        dfs(graph,start,visited);
+        return 0;
 }

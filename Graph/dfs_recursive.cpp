610c461f09d281007a25bd4ab808382eef715079
mayank-tripathik <mayanktripathik@gmail.com>
2016-10-23 00:18:09 +0530

    Create dfs_recursive.cpp

---
+++ Graph/dfs_recursive.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dfs(vector<list<int> > &graph, int u, vector<bool> &visited)
+{
+	visited[u]=true;
+	list<int>::iterator itr;
+	for(itr=graph[u].begin();itr!=graph[u].end();itr++){
+		int v=(*itr);
+		if(!visited[v])
+			dfs(graph,v,visited);
+        else
+            cout<<"visited:"<<u<<endl;
+	}
+}
+
+int main()
+{
+	int u,v,start,nodes,edges;
+	cin>>nodes>>edges;
+    vector<list<int> >graph(nodes+1);
+    vector<bool> visited(nodes+1,false);
+    for(int i=1;i<=edges;i++){
+    	cin>>u>>v;
+    	graph[u].push_back(v);
+    	graph[v].push_back(u);
+    }
+    cin>>start;
+    dfs(graph,start,visited);
+    return 0;
+}

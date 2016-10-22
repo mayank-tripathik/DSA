4bfd5ecda0fcd3c0e60cf4f8f1c24ac5d06300ab
mayank-tripathik <mayanktripathik@gmail.com>
2016-10-22 23:20:26 +0530

    Update krushkal.cpp

--- Graph/krushkal.cpp
+++ Graph/krushkal.cpp
@@ -39,7 +39,7 @@ void _union(vector<pair<int,int> > &sub,int u,int v){
 bool mycom(const pair<int, pair<int, int> > &v1, const pair<int, pair<int, int> > &v2){
     if(v1.first<v2.first)
         return true;
-    else if(v1.first<v2.first)
+    else if(v1.first>v2.first)
         return false;
     else
     {
@@ -53,41 +53,27 @@ bool mycom(const pair<int, pair<int, int> > &v1, const pair<int, pair<int, int> 
 }
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int edges,nodes,u,v,weight,start,sum=0;
     cin>>nodes>>edges;
-    vector<list<pair<int,int> > >graph(nodes+1);
-    vector<pair<int,pair<int,int> > > graphdata;
-    //vector<pair<int,pair<int,int> > > graphdata;
+    vector<pair<int,pair<int,int> > > graph;
     vector<pair<int,int> > subset(nodes+1);
     for(int i=1;i<=edges;i++){
         cin>>u>>v>>weight;
-        graph[u].push_back(make_pair(v,weight));
-        graphdata.push_back(make_pair(weight,make_pair(u,v)));
+        graph.push_back(make_pair(weight,make_pair(u,v)));
     }
     cin>>start;
-    /*for(int i=1;i<=nodes;i++){
-        list<pair<int,int> >::iterator itr;
-        for(itr=graph[i].begin();itr!=graph[i].end();itr++){
-            cout<<i<<" "<<(*itr).first<<" "<<(*itr).second<<endl;
-        }
-    }*/
     vector<pair<int,pair<int,int> > >::iterator it;
-    sort(graphdata.begin(),graphdata.end(),mycom);
+    sort(graph.begin(),graph.end(),mycom);
     int i=1;
     initialize(subset,nodes);
-    //pr(subset,nodes);
-    for(it=graphdata.begin();it!=graphdata.end() && i<=edges-1;it++)
+    for(it=graph.begin();it!=graph.end() && i<=edges-1;it++)
     {
-        //cout<<(*it).first<<" "<<(*it).second.first<<" "<<(*it).second.second<<endl;
         int u=(*it).second.first;
         int v=(*it).second.second;
-        //cout<<"u and v: "<<u<<" and "<<v<<endl;
         bool create_cycle=find(subset,u,v);
         if(!create_cycle)
         {
             _union(subset,u,v);
-            //pr(subset,nodes);
             sum+=(*it).first;
             i++;
         }
